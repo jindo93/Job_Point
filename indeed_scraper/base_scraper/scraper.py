0d94ba7b3072e48e17566c3710f3596fbf25cebc
jindo93 <doyj2012@gmail.com>
2019-04-22 13:39:22 -0400

    update indeed_page to use celery and rabbitmq

--- indeed_scraper/base_scraper/scraper.py
+++ indeed_scraper/base_scraper/scraper.py
@@ -7,23 +7,26 @@ from selenium.webdriver.chrome.options import Options
 from schema import input_job_data
 from indeed_page import IndeedPage
 from auth import URL, NEO4J_USR, NEO4J_PW
-
+import os
 
 graph = Graph(URL, auth=(NEO4J_USR, NEO4J_PW))
-
-# Internal Function
+cwd = os.getcwd()
 
 
 def get_driver():
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome(options=options)
-    driver.set_window_size(1440, 900)
-
-    print('Got driver! \n')
-    return driver
+    try:
+        options = Options()
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        driver = webdriver.Chrome(options=options)
+        driver.set_window_size(1440, 900)
+        # driver = webdriver.Chrome()
+        return driver
+    except:
+        with open(cwd+"/log.txt", "a") as f:
+            f.write('FAILED TO GET DRIVER! \n')
+        f.close()
 
 
 def scrape_jobs(job_title):
@@ -42,23 +45,30 @@ def scrape_jobs(job_title):
         while ip.next_page_button:
             job_details.extend(ip.get_job_details(ip.get_job_cards))
             ip.next_page_button.click_next_page()
-            try:
+            if ip.check_popup:
                 ip.popup.click()
-            except:
-                pass
-            if len(job_details) > 300:
+
+            if len(job_details) > 50:
+                with open(cwd+'/log.txt', 'a') as f:
+                    f.write("{num_jobs} job details added for {job_title} \n".format(
+                        num_jobs=len(job_details), job_title=job_title))
+                f.close()
                 input_job_data(graph, job_details)
                 job_details = []
-                # break  # TODO Comment out for production mode
+                break  # TODO Comment out for production mode
         if len(job_details) > 0:
             input_job_data(graph, job_details)
         job_details = []
-        print('All jobs titled <' + job_title + '> have been scraped')
+        with open(cwd+'/log.txt', 'a') as f:
+            f.write('All jobs titled <' + job_title + '> have been scraped \n')
+        f.close()
         browser.quit()
         return True
     except:
         browser.quit()
-        return False
+        with open(cwd+'/log.txt', 'a') as f:
+            f.write('ERROR SCRAPING {job_title}'.format(job_title=job_title))
+        f.close()
 
 
 if __name__ == '__main__':
--- indeed_scraper/celery_scraper/indeed_page.py
+++ indeed_scraper/celery_scraper/indeed_page.py
@@ -1,154 +1,151 @@
 #!/usr/bin/env python3
 
-import celery
+import os
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-from namedtuples.locator import Locator
-from utils.auth import BROKER, BACKEND
+from base_element import BaseElement
+from base_page import BasePage
+from locator import Locator
 
-url = 'https://www.indeed.com'
-
-app = celery.Celery('indeed_page1.py',
-                    broker=BROKER,
-                    backend=BACKEND)
+cwd = os.getcwd()
 
 
-def go(driver):
-    driver.get(url)
+class IndeedPage(BasePage):
+    url = 'https://indeed.com'
 
-
-def job_search(driver):
-    locator = Locator(by=By.ID, value='text-input-what')
-    element = get_element(driver, locator)
-    return element
-
-
-def input_text(element, txt):
-    try:
-        element.send_keys(txt)
-        return None
-    except:
-        return False
-
-
-def click_job_search_button(driver):
-    locator = Locator(by=By.CLASS_NAME, value='icl-Button')
-    element = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable(locator=locator)
-    )
-    element.click()
-    return None
-
-
-def next_page_button(driver):
-    try:
-        locator = Locator(by=By.LINK_TEXT, value="Next »")
-        element = get_element(driver, locator)
+    @property
+    def search_input(self):
+        locator = Locator(by=By.ID, value='text-input-what')
+        element = BaseElement(
+            driver=self.driver,
+            locator=locator
+        )
+        element.find_elem()
         return element
-    except:
-        return False
 
+    @property
+    def search_location_text(self):
+        locator = Locator(by=By.ID, value='text-input-where')
+        element = BaseElement(
+            driver=self.driver,
+            locator=locator
+        )
+        element.find_elem()
+        return element
 
-def click_next_page_button(driver):
-    locator = Locator(
-        by=By.XPATH, value="//div[@class='pagination']/a"
-    )
-    elements = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable(locator=locator)
-    )
-    element = elements[-1]
-    element.click()
-    return None
+    @property
+    def search_job_button(self):
+        locator = Locator(by=By.CLASS_NAME, value='icl-Button')
+        element = BaseElement(
+            driver=self.driver,
+            locator=locator
+        )
+        element.find_elem()
+        return element
 
+    @property
+    def next_page_button(self):
+        locator = Locator(
+            by=By.XPATH, value="//div[@class='pagination']/a"
+        )
+        element = BaseElement(
+            self.driver, locator
+        )
+        element.find_next_page()
+        return element
 
-def click_popup(driver):
-    locator = Locator(by=By.XPATH, value="//div[@id='popover-x']/a")
-    element = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable(locator=locator)
-    )
-    element.click()
-    return None
+    @property
+    def check_next_page(self):
+        locator = Locator(
+            by=By.XPATH, value="//div[@class='pagination']/a"
+        )
+        element = BaseElement(
+            self.driver, locator
+        )
+        return element.check_next_page()
 
+    @property
+    def popup(self):
+        locator = Locator(
+            by=By.XPATH, value="//div[@id='popover-x']/a")
+        element = BaseElement(self.driver, locator)
+        element.find_elem()
+        return element
 
-def get_element(driver, locator):
-    element = WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located(locator=locator)
-    )
-    print('element: ', element)
-    return element
-
-
-def get_elements(driver, locator):
-    elements = WebDriverWait(driver, 10).until(
-        EC.visibility_of_all_elements_located(locator=locator)
-    )
-    return elements
-
-
-def popup(driver):
-    try:
+    @property
+    def check_popup(self):
         locator = Locator(by=By.XPATH, value="//div[@id='popover-x']/a")
-        element = get_element(driver, locator)
-        return element
-    except:
-        return False
+        element = BaseElement(self.driver, locator)
+        return element.check_popup()
 
+    @property
+    def get_job_cards(self):
+        locator = Locator(by=By.CLASS_NAME, value='jobsearch-SerpJobCard')
+        elements = BaseElement(
+            self.driver, locator
+        )
+        elements.find_elems()
+        return elements.web_element  # returns a list of job card web elements
 
-@app.task
-def get_job_cards(driver):
-    locator = Locator(by=By.CLASS_NAME, value='jobsearch-SerpJobCard')
-    elements = get_elements(driver, locator)
-    return elements
+    def get_job_details(self, web_elements):
+        job_details = [[self.get_job_title(e),
+                        self.get_job_post_url(e),
+                        self.get_company(e),
+                        self.get_location(e),
+                        self.get_summary(e)] for e in web_elements]
+        return job_details
 
+    def get_job_title(self, web_element):
+        try:
+            job_title = web_element.find_element(
+                By.CLASS_NAME, 'jobtitle').text
+            return job_title
+        except:
+            with open(cwd+'/log.txt', 'a') as f:
+                f.write('No job title!')
+            f.close()
 
-@app.task
-def get_job_title(web_element):
-    job_title = web_element.find_element(By.CLASS_NAME, 'jobtitle').text
-    return job_title
+    def get_job_post_url(self, web_element):
+        try:
+            url = web_element.find_element(
+                By.CLASS_NAME, 'jobtitle').get_attribute('href')
+            if url == None:
+                url = web_element.find_element(
+                    By.CLASS_NAME, 'turnstileLink').get_attribute('href')
+            return url
+        except:
+            with open(cwd+'/log.txt', 'a') as f:
+                f.write('No post url..')
+            f.close()
 
+    def get_company(self, web_element):
+        try:
+            company = web_element.find_element(By.CLASS_NAME, 'company').text
+            return company
+        except:
+            company = "Unnamed"
+            with open(cwd+'/log.txt', 'a') as f:
+                f.write('No company name..')
+            f.close()
+            return company
 
-@app.task
-def get_job_post_url(web_element):
-    url = web_element.find_element(
-        By.CLASS_NAME, 'jobtitle'
-    ).get_attribute('href')
+    def get_location(self, web_element):
+        try:
+            location = web_element.find_element(By.CLASS_NAME, 'location').text
+            return location
+        except:
+            with open(cwd+'/log.txt', 'a') as f:
+                f.write('No location..')
+            f.close()
 
-    if url == None:
-        url = web_element.find_element(
-            By.CLASS_NAME, 'turnstileLink'
-        ).get_attribute('href')
-    return url
-
-
-@app.task
-def get_company(web_element):
-    try:
-        company = web_element.find_element(By.CLASS_NAME, 'company').text
-        return company
-    except:
-        company = "Unnamed"
-        return company
-
-
-@app.task
-def get_location(web_element):
-    location = web_element.find_element(By.CLASS_NAME, 'location').text
-    return location
-
-
-# def click(driver, locator):
-#     element =WebDriverWait(driver, 10).until(
-#         EC.element_to_be_clickable(locator)
-#     )
-#     element.click()
-#     return None
-
-# def get_job_details(web_elements):
-#     job_details = [[get_job_title(e),
-#                     get_job_post_url(e),
-#                     get_company(e),
-#                     get_location(e)] for e in web_elements]
-#     return job_details
+    def get_summary(self, web_element):
+        try:
+            summary = web_element.find_element(By.CLASS_NAME, 'summary').text
+            return summary
+        except:
+            with open(cwd+'/log.txt', 'a') as f:
+                f.write('No summary..')
+            f.close()
