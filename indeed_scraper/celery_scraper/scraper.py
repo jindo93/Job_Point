74741502b4ae30eeb66ad5fb66af86624d029d44
jindo93 <doyj2012@gmail.com>
2019-04-22 13:39:56 -0400

    update scraper to use celery and rabbitmq

--- indeed_scraper/celery_scraper/scraper.py
+++ indeed_scraper/celery_scraper/scraper.py
@@ -1,55 +1,78 @@
 #!/usr/bin/env python3
 
-from py2neo import Graph
+from celery import Celery
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import celery
-from celery import group
 
 from schema import input_job_data
-import indeed_page as ip
-from utils.auth import NEO4J_USR, NEO4J_PW
+from indeed_page import IndeedPage
+from auth import BROKER
+import logger as Log
 
-graph = Graph("bolt://localhost:7687", auth=(NEO4J_USR, NEO4J_PW))
+app = Celery('scraper', broker=BROKER)
 
-if __name__ == '__main__':
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
 
-    driver = webdriver.Chrome(options=options)
-    driver.set_window_size(1440, 900)
+def get_driver():
+    try:
+        options = Options()
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        driver = webdriver.Chrome(options=options)
+        driver.set_window_size(1440, 900)
+        # driver = webdriver.Chrome()
+        return driver
+    except:
+        Log.log_message('FAILED TO GET DRIVER!')
 
-    job_title = 'data scientist'
 
-    ip.go(driver)
-    ip.input_text(ip.job_search(driver), job_title)
-    ip.click_job_search_button(driver)
+def save_job_data(job_title, state, job_details):
+    try:
+        Log.job_data_to_csv(job_details, state)
+        # input_job_data(job_details, state) FIXME
+        Log.log_message("{state}, {num_jobs}, {job_title}  job data saved!".format(
+            num_jobs=len(job_details), job_title=job_title, state=state))
+    except:
+        Log.log_message("ERROR SAVING DATA: " + state + ' ' + job_title)
 
-    job_details = []
 
-    while ip.next_page_button(driver):
-        job_cards = ip.get_job_cards.delay(driver).get()
+@app.task
+def scrape_jobs(job_title, state):
+    Log.instantiate_csv(state)
+    browser = get_driver()
+    try:
+        ip = IndeedPage(driver=browser)
+        ip.go()
+        ip.search_input.input_text(job_title)
+        loc = ip.search_location_text
+        loc.click()
+        loc.manual_clear()
+        loc.input_text(state)
+        ip.search_job_button.click()
 
-        job_details.extend(
-            [group(
-                [ip.get_job_title.s(e),
-                 ip.get_job_post_url.s(e),
-                 ip.get_company.s(e),
-                 ip.get_location.s(e)] for e in job_cards)().get()]
-        )
+        job_details = []
 
-        ip.click_next_page_button(driver)
+        while ip.check_next_page:
+            job_details.extend(ip.get_job_details(ip.get_job_cards))
+            ip.next_page_button.click_next_page()
+            if ip.check_popup:
+                ip.popup.click()
 
-        if ip.popup(driver):
-            ip.click_popup(driver)
-
-        if len(job_details) > 100:
-            input_job_data(graph, job_details)
+            if len(job_details) > 30:
+                save_job_data(job_title, state, job_details)
+                job_details = []
+                break  # TODO Comment out for production mode
+        if len(job_details) > 0:
+            save_job_data(job_title, state, job_details)
             job_details = []
-            break
-    input_job_data(graph, job_details)
-    job_details = []
-    print('All jobs titled <' + job_title + '> have been scraped')
-    driver.close()
+
+        Log.log_message("COMPLETED JOB SCRAPING: " +
+                        job_title + ' ' + state + '\n')
+        browser.quit()
+        return None
+    except:
+        Log.log_message("ERROR SCRAPING DATA: " +
+                        state + ' ' + job_title + '\n')
+        browser.quit()
+        return None
