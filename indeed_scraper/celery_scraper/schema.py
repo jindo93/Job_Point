0984a4e4f8066d1661be9009b61155f97d9fd60e
jindo93 <doyj2012@gmail.com>
2019-04-22 13:55:02 -0400

    add modified schema

--- indeed_scraper/celery_scraper/schema.py
+++ indeed_scraper/celery_scraper/schema.py
@@ -1,17 +1,60 @@
 #!/usr/bin/env python3
 
-from py2neo import Node, Relationship
+from neo4j import GraphDatabase
+import logger as Log
+
+from auth import URL, NEO4J_USR, NEO4J_PW
 
 
-def input_job_data(graph, job_details):
-    for job_detail in job_details:
-        job = Node("Job", name=job_detail[0], post_url=job_detail[1])
-        company = Node("Company", name=job_detail[2])
-        location = Node("Location", name=job_detail[3])
+driver = GraphDatabase.driver(URL, auth=(NEO4J_USR, NEO4J_PW))
 
-        graph.merge(job, "Job", "post_url")
-        graph.merge(company, "Company", "name")
-        graph.merge(location, "Location", "name")
-        HAS = Relationship.type("HAS")
-        LOCATED = Relationship.type("LOCATED")
-        graph.merge(HAS(company, job) | LOCATED(company, location))
+
+def input_source():
+    try:
+        with driver.session() as session:
+            session.run("CREATE (s:Source {name: 'Indeed'})")
+        return True
+    except:
+        return False
+
+
+def input_job_detail(job_detail, state):
+    with driver.session() as session:
+        session.run("MATCH (s:Source) WHERE s.name = 'Indeed'"
+                    "MERGE (j:Job {name: {j_name}, summary: {summary}, post_url: {p_url}})"
+                    "MERGE (c:Company {name: {c_name}})"
+                    "MERGE (l:Location {name: {l_name}, state: {state}})"
+                    "MERGE (s)<-[:FROM]-(j)"
+                    "MERGE (l)<-[:LOCATED]-(j)<-[:HAS]-(c)",
+                    j_name=job_detail[0], summary=job_detail[4], p_url=job_detail[1],
+                    c_name=job_detail[2], l_name=job_detail[3], state=state)
+    return None
+
+
+def input_job_data(job_details, state):
+    try:
+        with driver.session() as session:
+            for job_detail in job_details:
+                job_detail.append(state)
+                session.run("MATCH (s:Source) WHERE s.name = 'Indeed'"
+                            "MERGE (j:Job {name: {j_name}, summary: {summary}, post_url: {p_url}})"
+                            "MERGE (c:Company {name: {c_name}})"
+                            "MERGE (l:Location {name: {l_name}, state: {state}})"
+                            "MERGE (s)<-[:FROM]-(j)"
+                            "MERGE (l)<-[:LOCATED]-(j)<-[:HAS]-(c)",
+                            j_name=job_detail[0], summary=job_detail[4], p_url=job_detail[1],
+                            c_name=job_detail[2], l_name=job_detail[3], state=job_detail[5])
+        session.close()
+        return None
+    except:
+        Log.log_message("FAILED TO INPUT DATA: " + state)
+
+# def input_job_data(job_details, state):
+#     try:
+#         for job_detail in job_details:
+#             input_job_detail(job_detail, state)
+#     except:
+#         Log.log_message("FAILED TO INPUT")
+#         with open(cwd+'/log.txt', 'a') as f:
+#             f.write("FAILED TO INPUT DATA \n")
+#         f.close()
